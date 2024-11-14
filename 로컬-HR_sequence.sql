e664aab2b38e645d37336224fdc04fb5d1fa6f0b
Taewon99 <2xo12xo1@gmail.com>
2024-11-14 09:39:29 +0900

    [modified] add index informations and view problems

--- 로컬-HR_sequence.sql
+++ 로컬-HR_sequence.sql
@@ -52,3 +52,59 @@ select * from user_sequences;
 
 -- sequence 삭제
 drop sequence dept_seq;
+
+-- 데이터 딕셔너리에서 index 확인
+select * from user_ind_columns where table_name = 'EMPLOYEES';
+
+SELECT * FROM EMPLOYEES WHERE EMPLOYEE_ID = 100; 
+DROP TABLE EMP10;
+CREATE TABLE EMP10
+AS
+SELECT * FROM EMPLOYEES WHERE 1=1; 
+SELECT * FROM EMP10 WHERE EMPLOYEE_ID = 100; 
+
+-- 인덱스 생성하기 
+SELECT * FROM user_ind_columns WHERE TABLE_NAME = 'EMP10';
+CREATE UNIQUE INDEX EMP10_EMPLOYEE_IX
+ON EMP10(EMPLOYEE_ID); 
+
+-- 인덱스 삭제하기
+DROP INDEX EMP10_EMPLOYEE_IX;
+--
+SELECT * FROM USER_SOURCE; 
+
+
+
+-- 사원 번호와 사원명과 부서명과 부서의 위치를 출력하는 뷰(VIEW_LOC)를 작성하라
+create view view_loc
+as
+select employee_id, first_name, department_name, location_id
+from employees join departments on employees.department_id = departments.department_id;
+
+select * from view_loc;
+
+--  30번 부서 소속 사원의 이름과 입사일과 부서명을 출력하는 뷰(VIEW_DEPT30)를 작성하라.
+create view view_dept30
+as
+select first_name, hire_date, department_name
+from employees join departments on employees.department_id = departments.department_id
+where employees.department_id = 30;
+
+select * from view_dept30;
+
+-- 부서별 최대 급여 정보를 가지는 뷰(VIEW_DEPT_MAXSAL)를 생성하라.
+create view view_dept_maxsal
+as
+select department_id, max(salary) as max_salary
+from employees
+group by department_id
+order by department_id;
+
+select * from view_dept_maxsal;
+
+-- 급여를 많이 받는 순서대로 3명만 출력하는 뷰(VIEW_SAL_TOP3)와 인라인 뷰로 작성하라.
+select rownum, first_name, salary
+from (select first_name, salary
+        from employees
+        order by salary desc)
+where rownum <=3;        
